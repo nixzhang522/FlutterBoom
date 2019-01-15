1c4c924a4e3b7743562938c129c827ce2388700c
Nix <351235445@qq.com>
2019-01-15 10:08:24 +0800

    completed recommend course

--- lib/module/home/home.dart
+++ lib/module/home/home.dart
@@ -296,6 +296,136 @@ class MovieState extends State<Home> {
       child: new Stack(
         children: <Widget>[
           new Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(16, 10, 0, 0),
+                          child: Text(
+                            "全年考研VIP全程班-英语",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w700,
+                                color: Color(0xff222626)),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(16, 4, 0, 0),
+                          child: Text(
+                            "更新至第1集",
+                            style: TextStyle(
+                                fontSize: 13, color: Color(0xff808080)),
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.fromLTRB(16, 4, 0, 0),
+                              height: 18,
+                              padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                              color: Color(0xfffcd433),
+                              child: Text(
+                                "外教",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 11, color: Color(0xff222626)),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(16, 4, 0, 0),
+                              height: 18,
+                              padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                              color: Color(0xfffcd433),
+                              child: Text(
+                                "精品",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 11, color: Color(0xff222626)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 123,
+                      height: 94,
+                      margin: EdgeInsets.fromLTRB(0, 10, 16, 0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(7)),
+                        image: DecorationImage(
+                            image: NetworkImage("http://pic2.ooopic.com/12/58/16/15bOOOPICae.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  color: Color(0xfff8f8f8),
+                  height: 42,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+                        child: Row(
+                          children: <Widget>[
+                            ClipOval(
+                              child: SizedBox(
+                                width: 32.0,
+                                height: 32.0,
+                                child: Image(
+                                  image: NetworkImage("http://pic2.ooopic.com/12/58/16/15bOOOPICae.jpg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(10, 0, 0, 0), 
+                              child: Text("Vica", style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                                color: Color(0xff222626)
+                              ),),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                        child: RichText(
+                          text: TextSpan(
+                            text: "RMB ",
+                            style: TextStyle(
+                              color: Color(0xff222626),
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: "1600",
+                                style: TextStyle(
+                                  color: Color(0xff222626),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700
+                                )
+                              )
+                            ]
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
             decoration: BoxDecoration(
               color: Color(0xffffffff),
               borderRadius: BorderRadius.all(new Radius.circular(6)),
